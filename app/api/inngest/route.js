940f59f0db8985cbed4f47976e2a23c2a4daa9ed
yasinarafat5353 <yasinrafat.cid.bd@gmail.com>
2025-11-24 20:05:13 +0600

    add json2

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,7 @@
+
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 import { serve } from "inngest/next";
-import { inngest } from "@/lib/inngest";
-import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+
 
 // This route handles Inngest events
 export const { GET, POST, PUT } = serve({
