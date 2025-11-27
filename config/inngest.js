0bde44d76e4c271b9dbc61de37d5888bf3d80841
yasinarafat5353 <yasinrafat.cid.bd@gmail.com>
2025-11-27 06:44:16 +0600

    Create a client to send and receive events

--- config/inngest.js
+++ config/inngest.js
@@ -25,3 +25,35 @@ export const syncUserCreation = Inngest.createFunction(
         await User.create(userData)
     }
 )
+// Inngrst Function to updata in database
+export const syncUserUpation = Inngest.createFunction(
+    {
+        id: 'update-user-from-clerk '
+    },
+    {event: 'clerk/user.updated'},
+    async({event}) => {
+          const {id , first_name, last_name, email_addresses, image_url } = event.data
+        const userData ={
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            imageUrl : image_url ,
+        }
+        await connectDB()
+        await User.findByIdAndUpdate(id, userData)
+    }
+)
+
+// Inngest Function to delet database
+export const syncUserDeletion = Inngest.createFunction(
+    {
+        id: 'delete-user-with-clerk'
+    },
+    {event:'clerk/userAgent.deleted' },
+    async({event}) => {
+        const {id } = event.data
+
+        await connectDB()
+        await User.findOneAndDelete(id)
+    }
+) 
